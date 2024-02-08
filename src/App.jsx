68596d45c73efc72d7242aa55602a6a040eb6d41
PrinceTyagia <veertyagia@gmail.com>
2024-02-08 13:13:36 +0530

    five commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-// import "./App.css";
-import CustomerTitleBar from "./components/CustomerTitleBar";
+
 import CustomEditTable from "./components/CustomEditTable";
-import TabLikeEditing from "./TabLikeEditing";
-import MultiWindow from "./components/MultiWindow";
-import { saveWindowState, StateFlags  } from "tauri-plugin-window-state-api";
-import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
+
+import { getName ,getVersion  } from '@tauri-apps/api/app';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [version, setVersion] = useState("");
 
- 
+  
+  useEffect(() =>{
+    const getVersionApp = async() => {
+      const appVersion = await getVersion();
+      const appName = await getName();
+      setVersion(appVersion);
+      setName(appName)
+    }
+    getVersionApp()
+
+  },[]);
+  const callNotification = () => {
+    toast.info(<div>{name}{version}<button></button></div>,{
+      position:"bottom-right",
+      autoClose:false
+    })
+  }
+
+
 
 
 
 
   return (
     <div className="container">
+      <ToastContainer hideProgressBar/>
      {/* <p>Custom TitleBar</p>
      <CustomerTitleBar/> */}
      <CustomEditTable/>
      {/* <TabLikeEditing/> */}
      {/* <MultiWindow/> */}
+     <p>{name}</p>
+     <button onClick={callNotification}>trigger</button>
     </div>
   );
 }
